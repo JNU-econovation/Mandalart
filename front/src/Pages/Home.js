8ae2e2c52a6b243c001d2c9294b7cb2713f32827
mywnajsldkf <mywnajsldkf@gmail.com>
2020-01-04 22:30:07 +0900

    add:context why:로그인을 전역적으로 관리하기 위해서 context API를 사용하기로함.

--- front/src/Pages/Home.js
+++ front/src/Pages/Home.js
@@ -4,12 +4,37 @@ import Login from "../Components/Login/Login";
 import { Router, BrowserRouter } from "react-router-dom";
 
 class Home extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      logged: false,
+      onLogin: this.onLogin,
+      onLogout: this.onLogout
+    };
+  }
+
+  // Login Func
+  onLogin = () => {
+    this.setState({
+      logged: true
+    });
+  };
+
+  // Logout Func
+  onLogout = () => {
+    this.setState({
+      logged: false
+    });
+  };
+
   render() {
+    const { logged, onLogout } = this.state;
+
     return (
-        <container>
-          <Header />
-          <Login />
-        </container>
+      <container>
+        <Header logged={logged} onLogout={onLogout} />
+        <Login />
+      </container>
     );
   }
 }
