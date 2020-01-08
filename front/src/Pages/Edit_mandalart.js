d98d665dd5e9d0c643c8e069f4b7b8d004cb23cf
mywnajsldkf <mywnajsldkf@gmail.com>
2020-01-08 18:38:56 +0900

    add:edit-mandal form(All_mandal)
    All_mandal : 9개의 만다라트를 올린다.
    Main_mandal : 9개의 만다라트를 입력하는 input이 있는 page
    Specific_mandal : 9개의 만다라트 각각 하나씩 세부적인 내용을 입력하는
    page

--- front/src/Pages/Edit_mandalart.js
+++ front/src/Pages/Edit_mandalart.js
@@ -1,8 +1,15 @@
 import React, { Component } from "react";
+import Header from "../Components/Add_mandalart/Header";
+import All_mandal from "../Components/Edit_mandalart/All_mandal";
 
 class Edit_mandalart extends Component {
   render() {
-    return <div>Edit_mandalart</div>;
+    return (
+      <div>
+        <Header />
+        <All_mandal />
+      </div>
+    );
   }
 }
 
