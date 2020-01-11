dd3fc0b15a55dad359c2784192ec80c5a1a8196c
ummaeha <raya000@naver.com>
2020-01-11 12:47:06 +0900

    chore : pages/Guestpage.js , Guestpage.css
    detail : 로그인  버튼 위치 변경했습니다.

--- front/src/pages/Guestpage.js
+++ front/src/pages/Guestpage.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
 import MainHeader from "../components/MainHeader";
+import "./Guestpage.css";
 // import image1 from "../images/image1.png";
 // import image2 from "../images/image2.png";
 // import image3 from "../images/image3.png";
@@ -15,7 +16,7 @@ class Guestpage extends Component {
     return (
       <>
         <Link to="/login">
-          <button> 로그인</button>
+          <button class="button"> 로그인</button>
         </Link>
         <MainHeader
           image="https://media.wired.com/photos/5dee9dde524c380008bedf86/master/pass/Gear_bblue_16-inch-MacBook-Pro-SOURCE-Apple.jpg"
