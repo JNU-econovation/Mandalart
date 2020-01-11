273fdccd8e11f0dd5c88cb07c4804fd5366f65b0
mywnajsldkf <mywnajsldkf@gmail.com>
2020-01-11 16:15:13 +0900

    add : database.json
    database 정보를 담고 있는 database.json파일을 config폴더에 넣었고
    이정보를 .gitignore에 업데이트 시켰다.

--- back-end/app.js
+++ back-end/app.js
@@ -1,42 +1,12 @@
-var createError = require("http-errors");
-var express = require("express");
-var bodyParser = require("body-parser");
-var path = require("path");
-var cookieParser = require("cookie-parser");
-var logger = require("morgan");
+const express = require("express");
+const bodyParser = require("body-parser");
+const app = express();
+const port = process.env.PORT || 3001;
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
-var indexRouter = require("./routes/index");
-var usersRouter = require("./routes/users");
-
-var app = express();
-
-// view engine setup
-app.set("views", path.join(__dirname, "views"));
-app.set("view engine", "jade");
-
-app.use(logger("dev"));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, "public")));
-
-app.use("/", indexRouter);
-app.use("/users", usersRouter);
-
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
+app.get("/api/hello", (req, res) => {
+  res.send({ message: "Hello Express!" });
 });
 
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get("env") === "development" ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render("error");
-});
-
-module.exports = app;
+app.listen(port, () => console.log(`Listening on port ${port}`));
